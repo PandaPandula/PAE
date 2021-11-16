713b2e3d45171b68fd39bc863793847f2269fe10
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2021-11-16 14:00:13 +0100

    Item extends Parcelable

--- app/src/main/java/com/SagaLogistic/backend/models/Item.java
+++ app/src/main/java/com/SagaLogistic/backend/models/Item.java
@@ -1,4 +1,17 @@
 package com.SagaLogistic.backend.models;
 
-public interface Item {
+import android.os.Parcelable;
+
+public interface Item extends Parcelable {
+    String getKey();
+
+    void setKey(String key);
+
+    String getName();
+
+    void setName(String name);
+
+    Float getWeight();
+
+    void setWeight(Float weight);
 }
--- app/src/main/java/com/SagaLogistic/backend/models/ItemImpl.java
+++ app/src/main/java/com/SagaLogistic/backend/models/ItemImpl.java
@@ -1,4 +1,76 @@
 package com.SagaLogistic.backend.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 public class ItemImpl implements Item{
+
+    public static final Parcelable.Creator CREATOR = new Creator() {
+        @Override
+        public ItemImpl createFromParcel(Parcel in) {
+            return new ItemImpl(in);
+        }
+
+        @Override
+        public ItemImpl[] newArray(int size) {
+            return new ItemImpl[size];
+        }
+    };
+
+    private String key;
+    private String name;
+    private Float weight;
+
+    public ItemImpl(String key, String name, Float weight){
+        this.key = key;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public ItemImpl(String name, Float weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public ItemImpl(Parcel in){
+        this.key = in.readString();
+        this.name = in.readString();
+        this.weight = in.readFloat();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.key);
+        dest.writeString(this.name);
+        dest.writeFloat(this.weight);
+    }
 }
