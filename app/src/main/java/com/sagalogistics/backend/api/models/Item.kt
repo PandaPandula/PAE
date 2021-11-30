e06ee0405cd90d28eb6cde026a55ccfcbfca0669
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2021-11-30 16:28:02 +0100

    documentation test

--- app/src/main/java/com/sagalogistics/backend/api/models/Item.kt
+++ app/src/main/java/com/sagalogistics/backend/api/models/Item.kt
@@ -2,9 +2,27 @@ package com.sagalogistics.backend.api.models
 
 import java.io.Serializable
 
+/**
+ * An item
+ */
 interface Item : Serializable {
+    /**
+     * The key of the item in the database
+     */
     var key: String?
+
+    /**
+     * The name of the item
+     */
     var name: String
+
+    /**
+     * The weight of the item
+     */
     var weight: Float
+
+    /**
+     * An image representing the item
+     */
     var image: String?
 }
