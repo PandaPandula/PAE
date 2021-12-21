fb36725f0a7b364fb30de8744a0b4d4b07b9757d
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2021-12-21 14:08:24 +0100

    WeightCalculator.weightOfUserOrders

--- app/src/main/java/com/sagalogistics/backend/api/database/WeightCalculator.kt
+++ app/src/main/java/com/sagalogistics/backend/api/database/WeightCalculator.kt
@@ -1,7 +1,9 @@
 package com.sagalogistics.backend.api.database
 
+import com.sagalogistics.backend.api.models.Bar
 import com.sagalogistics.backend.api.models.Item
 import com.sagalogistics.backend.api.models.Order
+import com.sagalogistics.backend.api.models.User
 
 /**
  * Utility class used to calculate the weight of an order
@@ -16,6 +18,26 @@ class WeightCalculator private constructor(){
      * Class methods
      */
     companion object{
+        fun weightOfUserOrders(user: User): Pair<Float, Float> {
+            val barKeys = user.bars
+            val listOfBars = FutureHelper.getListOfKeys(barKeys, Bar::class)
+
+            return listOfBars.fold(Pair(0f, 0f), {
+                total, bar ->
+
+                val orderKeys = bar.orders
+                val listOfOrders = FutureHelper.getListOfKeys(orderKeys, Order::class)
+
+                val (lowerWeightBar, upperWeightBar) = listOfOrders.fold(Pair(0f, 0f), {
+                    totalOrder, order ->
+
+                    val (lowerWeightOrd, upperWeightOrd) = weightOfOrder(order)
+                    Pair(totalOrder.first + lowerWeightOrd, totalOrder.second + upperWeightOrd)
+                })
+                Pair(total.first + lowerWeightBar,total.second + upperWeightBar)
+            })
+        }
+
         /**
          * Calculates the total weight of an [order]
          *
@@ -23,23 +45,20 @@ class WeightCalculator private constructor(){
          * and multiply it by its amount in the [order]
          * after considering its [upper][Item.upperVariance] and [lower][Item.lowerVariance] bounds
          *
-         * The [first][Pair.first] element of the pair will be the upper bound of weight,
-         * and the [second][Pair.second] will be the lower one
+         * The [first][Pair.first] element of the pair will be the lower bound of weight,
+         * and the [second][Pair.second] will be the upper one
          */
         fun weightOfOrder(order: Order): Pair<Float, Float> {
             val itemKeys = order.items.keys
             val listOfItems = FutureHelper.getListOfKeys(itemKeys, Item::class)
 
-            var upperWeight = 0f
-            var lowerWeight = 0f
-            for(item in listOfItems) {
-                val (upperVariance, lowerVariance) = upperLowerWeightOfItem(order, item)
+            return listOfItems.fold(Pair(0f, 0f), {
+                (lowerWeight, upperWeight), item ->
 
-                upperWeight += upperVariance
-                lowerWeight += lowerVariance
-            }
+                val (lowerVariance, upperVariance) = upperLowerWeightOfItem(order, item)
 
-            return Pair(upperWeight, lowerWeight)
+                Pair(lowerWeight + lowerVariance, upperWeight + upperVariance)
+            })
         }
 
         /**
@@ -61,14 +80,14 @@ class WeightCalculator private constructor(){
          */
         private fun upperLowerWeightOfItem(order: Order, item: Item): Pair<Float, Float> {
             val weightOfItem = item.weight
-            val upperVarianceOfItem = item.upperVariance
             val lowerVarianceOfItem = item.lowerVariance
+            val upperVarianceOfItem = item.upperVariance
             val quantityOfItem = order.items[item.key]!!
 
-            val upperVariance = weightOfItem * (1 + upperVarianceOfItem) * quantityOfItem
             val lowerVariance = weightOfItem * (1 - lowerVarianceOfItem) * quantityOfItem
+            val upperVariance = weightOfItem * (1 + upperVarianceOfItem) * quantityOfItem
 
-            return Pair(upperVariance, lowerVariance)
+            return Pair(lowerVariance, upperVariance)
         }
     }
 }
