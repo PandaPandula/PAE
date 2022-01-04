85d668153a3c3eeb9cf44b0bbee43451d44173f2
Adriafu <adria.fuster@estudiantat.upc.edu>
2022-01-04 17:26:29 +0100

    Items info ORDER

--- app/src/main/java/com/sagalogistics/barorderactivity/BarOrderActivity.kt
+++ app/src/main/java/com/sagalogistics/barorderactivity/BarOrderActivity.kt
@@ -3,24 +3,31 @@ package com.sagalogistics
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sagalogistics.barorderactivity.adapters.CustomAdapter
 import com.sagalogistics.lib.database.FutureHelper
 import com.sagalogistics.lib.database.Repository
+import com.sagalogistics.lib.database.WeightCalculator
 import com.sagalogistics.lib.models.Bar
 
 
 class BarOrderActivity : Activity()
 {
     lateinit var mRecyclerView : RecyclerView
+    private lateinit var pesT : TextView
+    private lateinit var pesR : EditText
 
     private var barsList: List<Bar> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_order2)
+        pesR = findViewById(R.id.PesR)
+        pesT = findViewById(R.id.PesT)
         setUpRecyclerView()
     }
 
@@ -28,7 +35,16 @@ class BarOrderActivity : Activity()
         mRecyclerView = findViewById(R.id.recylerview)
         mRecyclerView.setHasFixedSize(true)
         mRecyclerView.layoutManager = LinearLayoutManager(this)
-        val barlist = Repository.getInstance().getUser("-Mq04Y51CY4YFjo7AiqA").get()!!.bars.map{
+        //Agafem l'usuari
+        val user = Repository.getInstance().getUser("-Mq04Y51CY4YFjo7AiqA").get()!!
+
+        //Calculem el weight
+        val weightT = WeightCalculator.weightOfUserOrders(user)
+        val medianweight = (weightT.first + weightT.second / 2).toInt()
+        pesT.text = medianweight.toString()
+
+        //Agafem els bars de l'usuari
+        val barlist = user.bars.map{
             Repository.getInstance().getBar(it).get()!!
         }
         barsList = barlist
--- app/src/main/java/com/sagalogistics/itemsorderactivity/adapters/CustomAdapter.kt
+++ app/src/main/java/com/sagalogistics/itemsorderactivity/adapters/CustomAdapter.kt
@@ -48,12 +48,12 @@ class CustomAdapter(private val items: ArrayList<Triple<Item, Int, String>>, pri
         holder.render(items[position])
 
         holder.view.findViewById<TextView>(R.id.plus).setOnClickListener {
-            val quant = items[position].second.dec()
+            val quant = holder.view.findViewById<TextView>(R.id.quantity).text.toString().toInt().dec()
             //Actualitzem valor a la base de dades
             val a = Repository.getInstance().getOrder(items[position].third).get()!!.updateItem(items[position].first.key!!,quant)
 
             //Actualitzem el valor en local
-            holder.view.findViewById<TextView>(R.id.quantity).text = quant.toString()
+            if (quant < 0) holder.view.findViewById<TextView>(R.id.quantity).text = quant.toString()
         }
     }
 
