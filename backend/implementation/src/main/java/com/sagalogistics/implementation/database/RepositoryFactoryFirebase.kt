383d00b196cbffea3f83edd1c4335fa2c033b2f7
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-01-13 17:41:52 +0100

    implementation.models documented

--- backend/implementation/src/main/java/com/sagalogistics/implementation/database/RepositoryFactoryFirebase.kt
+++ backend/implementation/src/main/java/com/sagalogistics/implementation/database/RepositoryFactoryFirebase.kt
@@ -2,19 +2,35 @@ package com.sagalogistics.implementation.database
 
 import com.sagalogistics.lib.database.*
 
+/**
+ * Implementation of [RepositoryFactory] using a Firebase database and the data accessor objects
+ * in this package
+ */
 class RepositoryFactoryFirebase : RepositoryFactory {
+    /**
+     * Returns an instance of an [ItemDAOFirebase]
+     */
     override fun createItemDAO(): ItemDAO {
         return ItemDAOFirebase()
     }
 
+    /**
+     * Returns an instance of an [OrderDAOFirebase]
+     */
     override fun createOrderDAO(): OrderDAO {
         return OrderDAOFirebase()
     }
 
+    /**
+     * Returns an instance of a [BarDAOFirebase]
+     */
     override fun createBarDAO(): BarDAO {
         return BarDAOFirebase()
     }
 
+    /**
+     * Returns an instance of a [UserDAOFirebase]
+     */
     override fun createUserDAO(): UserDAO {
         return UserDAOFirebase()
     }
